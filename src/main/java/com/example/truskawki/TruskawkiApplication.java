f1403acfc0c5253525a4891ab535cfb914f408b9
Kamil Rogowski <kamil.rogowski@upmenu.com>
2020-04-08 19:15:17 +0200

    init

---
+++ src/main/java/com/example/truskawki/TruskawkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.truskawki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruskawkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruskawkiApplication.class, args);
+	}
+
+}
